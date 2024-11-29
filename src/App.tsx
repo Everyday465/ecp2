92c90dec9069dc939900ea73b53385c1f47adb94
Everyday465 <110691604+Everyday465@users.noreply.github.com>
2024-11-29 14:33:36 +0800

    base

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
+
+  //template start
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -16,25 +18,82 @@ function App() {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
+  //template end
+
+  const [responseMessage, setResponseMessage] = useState<string>('');
+
+//lamba funtion async
+  const callLambda = async () => {
+    //enter api url here
+    const apiUrl = 'https://7ijq9dwucg.execute-api.us-east-1.amazonaws.com/dev/hello';
+  
+    //enter json request body if need
+    const requestBody = JSON.stringify({
+      key1: 'value1',
+      key2: 'value2',
+    });
+  
+    try {
+      const response = await fetch(apiUrl, {
+        //GET/POST/PUT/DEL
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          //maybe need auth header
+          // Add authorization headers if necessary
+        },
+        body: requestBody
+      });
+  
+      const data = await response.json();
+      console.log('Lambda Response:', data);
+    } catch (error) {
+      console.error('Error calling Lambda:', error);
+    }
+  };
+
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+    <div style={{ fontFamily: 'Arial, sans-serif', textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to the Simple Website</h1>
+      <button
+        className="btn1"
+        onClick={callLambda}
+      >
+        Button 1
+      </button>
+      <button
+        className="btn2"
+        onClick={() => alert('Button 2 clicked!')}
+      >
+        Button 2
+      </button>
+      <button
+        className="btn3"
+        onClick={() => alert('Button 3 clicked!')}
+      >
+        Button 3
+      </button>
+
+      <p id="responseMessage">{responseMessage}</p>
+    </div>
+    // <main>
+    //   <h1>My todos</h1>
+    //   <button onClick={createTodo}>+ new</button>
+    //   <ul>
+    //     {todos.map((todo) => (
+    //       <li key={todo.id}>{todo.content}</li>
+    //     ))}
+    //   </ul>
+    //   <div>
+    //     🥳 App successfully hosted. Try creating a new todo.
+    //     <br />
+    //     <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+    //       Review next step of this tutorial.
+    //     </a>
+    //   </div>
+    // </main>
+  ); 
 }
 
 export default App;
