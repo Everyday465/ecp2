a0e1a1e9e82b3af5ea17325e35fb33623499907e
yuanx1n <liyuanxin682@gmail.com>
2024-12-07 22:09:24 +0800

    allow guest and change react

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,7 +4,8 @@ export const storage = defineStorage({
   name: 'transcribeupload', // Name of your S3 bucket
   access: (allow) => ({
     'uploadfiles/*': [  // Define the folder path within the S3 bucket
-      allow.authenticated.to(['read', 'write'])  // Permissions for authenticated users
+      allow.authenticated.to(['read', 'write']) , // Permissions for authenticated users
+      allow.guest.to(['read','write']) // Permissions for unauthenticated users
     ],
   }),
 });
