aa493471cd5a452fcbbd74cf8044cbffb17d286a
Everyday465 <110691604+Everyday465@users.noreply.github.com>
2024-12-07 08:30:07 +0800

    cdk rest api,lambda & dynamo

--- my-cdk-project/bin/my-cdk-project.ts
+++ my-cdk-project/bin/my-cdk-project.ts
@@ -3,9 +3,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { MyCdkProjectStack } from '../lib/my-cdk-project-stack';
 import { S3BucketStack } from '../lib/s3-bucket-stack';
+import { RestApiAwsStack } from '../lib/rest-api-aws-stack';
 
 const app = new cdk.App();
 //new MyCdkProjectStack(app, 'MyCdkProjectStack');
+
 //creating an s3 bucket stack
 const s3_bucket_stack = new S3BucketStack(app,"s3Stack", {
     env: {
@@ -13,6 +15,15 @@ const s3_bucket_stack = new S3BucketStack(app,"s3Stack", {
       region: process.env.CDK_DEFAULT_REGION,
     },
   });
+
+const rest_api_aws_stack = new RestApiAwsStack(app,"restApiStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
 //reusing assets
-const bucket = s3_bucket_stack.bucket;
+// const bucket = s3_bucket_stack.bucket;
+// const restApi = rest_api_aws_stack.;
+
     
